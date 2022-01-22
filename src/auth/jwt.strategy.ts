3d7cf5bcc11ad57ddae78f92ebe55fe0b500569f
vitalii verdiiev <vitaliiverdiiev@gmail.com>
2022-01-23 01:31:18 +0200

    Added signup and signin

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
+import { JwtPayloadInterface } from './jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private userRepository: UsersRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'topSecret51',
+    });
+  }
+
+  async validate(payload: JwtPayloadInterface): Promise<User> {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
